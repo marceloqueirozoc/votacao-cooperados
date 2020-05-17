ae87530a354e1d5a1e992eb84b38fe989dac6e11
Marcelo Constantino <marceloqueirozoc@gmail.com>
2020-05-17 12:52:04 -0300

    Usuário e Pauta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
-const app = express;
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false}));
+
+require('../controllers/authController')(app);
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000!');
+  });
